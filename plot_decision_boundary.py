4f4313431a7ac38c43411257b369193016610cd1
Jayveersinh-Raj <69463767+Jayveersinh-Raj@users.noreply.github.com>
2022-06-24 20:21:52 +0530

    Update plot_decision_boundary.py

--- plot_decision_boundary.py
+++ plot_decision_boundary.py
@@ -1,9 +1,9 @@
    
-    import numpy as np
-    import matplotlib.pyplot as plt
+     import numpy as np
+     import matplotlib.pyplot as plt
 
-    # Plots the decision boundary created by a model predicting on X.
-    def plot_decision_boundary(model, X, y):
+     # Plots the decision boundary created by a model predicting on X.
+     def plot_decision_boundary(model, X, y):
     # Define the axis boundaries of the plot and create a meshgrid
       x_min, x_max = X[:, 0].min() - 0.1 , X[:, 0].max() + 0.1
       y_min, y_max = X[:, 0].min() - 0.1, X[:, 1].max() + 0.1
@@ -31,7 +31,7 @@
         y_pred = np.round(y_pred).reshape(xx.shape)
  
        # Plot the boundary
-     plt.contourf(xx, yy, y_pred, cmap = plt.cm.RdYlBu, alpha = 0.7)
-     plt.scatter(X[:, 0], X[:,1], c= y, cmap = plt.cm.RdYlBu)
-     plt.xlim(xx.min(), xx.max())
-     plt.ylim(yy.min(), yy.max())
+      plt.contourf(xx, yy, y_pred, cmap = plt.cm.RdYlBu, alpha = 0.7)
+      plt.scatter(X[:, 0], X[:,1], c= y, cmap = plt.cm.RdYlBu)
+      plt.xlim(xx.min(), xx.max())
+      plt.ylim(yy.min(), yy.max())
